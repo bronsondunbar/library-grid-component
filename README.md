0062bf1d5f704bbe7985665b53a32088affae561
Bronson Dunbar <bronson@bronsondunbar.com>
2018-06-14 12:56:19 +0200

    Updates

--- README.md
+++ README.md
@@ -44,11 +44,14 @@ render () {
 
 	return (
 		<GridComponent
-			gridItems={gridData}
-			gridItemColour="#ffffff"
-			gridItemFill="#2bbde0"
-			gridItemBorder={true}
-			gridItemBorderColour="#2e3c54"
+			gridItems={data}
+			gridItemMinWidth="100"
+			gridItemLayout="1x1"
+			gridItemColour="#262626"
+			gridItemFill="#ffffff"
+			gridItemShadow={true}
+			gridItemBorder={false}
+			gridItemBorderRadius="5"
 			gridItemAlignment="center" />
 	)
 }
@@ -58,8 +61,12 @@ render () {
 | Prop                 | Values                                  |
 | :------------------- | :-------------------------------------- |
 | gridItems            | Object                                  |
+| gridItemMinWidth     | Min width for each item                 |
+| gridItemLayout       | 1x1, 2x1, 3x1, 4x1                      |
 | gridItemColour       | HEX colour code                         |
 | gridItemFill         | HEX colour code                         |
+| gridItemShadow       | true or false                           |
 | gridItemBorder       | true or false                           |
 | gridItemBorderColour | HEX colour code                         |
+| gridItemBorderRadius | Pixels                                  |
 | gridItemAlignment    | Alignment of data (left, right, center) |
--- src/Grid.js
+++ src/Grid.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React from 'react'
+import classNames from 'classnames'
+import DOMPurify from 'dompurify'
 
-import './style.css';
+import './style.css'
 
-const GridComponent = ({ gridItems, gridItemColour, gridItemFill, gridItemBorder, gridItemBorderColour, gridItemAlignment }) => {
+const GridComponent = ({ gridItems, gridItemMinWidth, gridItemLayout, gridItemColour, gridItemFill, gridItemBorder, gridItemBorderColour, gridItemBorderRadius, gridItemShadow, gridItemAlignment }) => {
+
+  let gridClass = classNames ({
+    grid: true,
+    'grid-4x1': gridItemLayout == '4x1',
+    'grid-3x1': gridItemLayout == '3x1',
+    'grid-2x1': gridItemLayout == '2x1'
+  })
+
+  let gridItemLayoutStyle = {
+    'gridTemplateColumns': gridItemLayout == '1x1' ? 'repeat(auto-fit, minmax(' + gridItemMinWidth + 'px, 1fr))' : null,
+  }
 
   let gridItemStyle = {
     background: gridItemFill,
     color: gridItemColour,
     border: gridItemBorder === true ? '1px solid' : 'none',
-    'border-color': gridItemBorderColour,
-    'justify-content': gridItemAlignment,
-    'align-items': 'center'
+    'borderColor': gridItemBorderColour,
+    'justifyContent': gridItemAlignment,
+    'alignItems': 'center',
+    'borderRadius': gridItemBorderRadius + 'px',
+    'minWidth': gridItemMinWidth + 'px',
+    'boxShadow': gridItemShadow === true ? '0px 3px 5px 0px rgba(0, 0, 0, 0.22)' : 'none' 
   }
 
   const items = gridItems.map(function (item, index) {
@@ -18,19 +34,19 @@ const GridComponent = ({ gridItems, gridItemColour, gridItemFill, gridItemBorder
       <div
         className="item"
         key={index}
-        style={gridItemStyle} >
-          {item.data}
+        style={gridItemStyle}
+        dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(item.data)}} >
       </div>
-    );
+    )
   })
 
   return (
     <div>
-      <div className="grid">
+      <div className={gridClass} style={gridItemLayoutStyle}>
         {items}
       </div>
     </div>
   )
 }
 
-export default GridComponent;
+export default GridComponent
